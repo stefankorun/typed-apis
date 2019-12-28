56a3d0ee16e24109ddcd6fecaa46e026f7002b6d
korun <stefankorun@outlook.com>
2019-12-28 19:47:35 +0100

    core: update libraries; fix jsdom error with google api; update tests

--- src/__tests__/general.ts
+++ src/__tests__/general.ts
@@ -1,3 +1,5 @@
+import {Facebook} from "../providers/Facebook";
+import {Microsoft} from "../providers/Microsoft";
 import {GoogleMaps} from "../providers/GoogleMaps";
 import {Google} from "../providers/Google";
 
@@ -5,11 +7,28 @@ describe('General usability', () => {
 
   describe('All scripts load', () => {
 
+    test('Facebook loading', async () => {
+      expect(await Facebook.load({
+        apiKey: 'api_key',
+        appId: 'n/a'
+      })).toBeDefined()
+    });
+    test('Microsoft loading', async () => {
+      expect(await Microsoft.load({
+        apiKey: 'api_key',
+        appId: 'n/a'
+      })).toBeDefined()
+    });
     test('GoogleMaps loading', async () => {
       expect(await GoogleMaps.load({
-        apiKey: 'api_key'
+        apiKey: 'api_key',
+        language: 'en'
       })).toBeDefined()
-    })
+    });
+    test('Google loading', async () => {
+      expect(await Google.load({
+        apiKey: 'api_key',
+      })).toBeDefined()
+    });
   })
-
 });
